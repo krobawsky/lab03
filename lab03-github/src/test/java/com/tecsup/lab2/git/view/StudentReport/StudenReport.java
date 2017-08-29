4afb43b7b8314bffe0c4931a556a90a850b2f87a
Alumno <Alumno@LAB402-15>
2017-08-29 10:51:53 -0500

    cambio 3

---
+++ lab03-github/src/test/java/com/tecsup/lab2/git/view/StudentReport/StudenReport.java
@@ -0,0 +1,14 @@
+package com.tecsup.lab2.git.view.StudentReport;
+
+public class StudenReport {
+	
+	public static void main( String[] args )
+    {
+        System.out.println( " Hola a gente" );
+    }
+    
+    public static void showMessage(  )
+    {
+        System.out.println( "Como estais?" );
+    }
+}
