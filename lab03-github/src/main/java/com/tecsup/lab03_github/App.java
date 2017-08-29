c7746db0966954dd5e017030457dcb6f589ab77a
Yonny <Alumno@LAB402-16>
2017-08-29 11:14:23 -0500

    Contruccion de Prueba de software

--- lab03-github/src/main/java/com/tecsup/lab03_github/App.java
+++ lab03-github/src/main/java/com/tecsup/lab03_github/App.java
@@ -8,11 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( " Hola a Todos " );
+        System.out.println( "Bienvenidos " );
     }
     
     public static void showMessage(  )
     {
-        System.out.println( "Mensaje de Bienvenida....!!" );
+        System.out.println( "Bienvenido a la clase de Contruccion de prueba de software....!!" );
     }
 }
