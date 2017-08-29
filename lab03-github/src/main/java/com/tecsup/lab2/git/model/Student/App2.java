7e29de8063cde16ef8f4bc79d5cc19f2dc3c0211
Yonny <Alumno@LAB402-16>
2017-08-29 10:40:18 -0500

    HOla mundo x2

---
+++ lab03-github/src/main/java/com/tecsup/lab2/git/model/Student/App2.java
@@ -0,0 +1,18 @@
+package com.tecsup.lab2.git.model.Student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( " Hola a Todos zzzzz " );
+    }
+    
+    public static void showMessage(  )
+    {
+        System.out.println( "Mensaje de Bienvenida a todo el mundo....!!" );
+    }
+}
